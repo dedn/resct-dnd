ef22cadf21eb904391436ea96621b897ff470cea
Mac <yurii.kyrylov.triare@gmail.com>
2021-06-10 16:28:15 +0300

    added base template

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import { DndProvider, useDrag, useDrop } from "react-dnd";
+import { HTML5Backend } from "react-dnd-html5-backend";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const MovableItem = ({setIsFirstColumn}) => {
+    const [{isDragging}, drag] = useDrag({
+        item: {name: 'Any custom name'},
+        type: 'Our first type',
+        end: (item, monitor) => {
+            const dropResult = monitor.getDropResult();
+            if(dropResult && dropResult.name === 'Column 1'){
+                setIsFirstColumn(true)
+            } else {
+                setIsFirstColumn(false);
+            }
+        },
+        collect: (monitor) => ({
+            isDragging: monitor.isDragging(),
+        }),
+    });
+
+    const opacity = isDragging ? 0.4 : 1;
+
+    return (
+        <div ref={drag} className='movable-item' style={{opacity}}>
+            We will move this item
+        </div>
+    )
 }
 
-export default App;
+const Column = ({children, className, title}) => {
+    const [, drop] = useDrop({
+        accept: 'Our first type',
+        drop: () => ({name: title}),
+    });
+
+    return (
+        <div ref={drop} className={className}>
+            {title}
+            {children}
+        </div>
+    )
+}
+
+export const App = () => {
+    const [isFirstColumn, setIsFirstColumn] = useState(true);
+
+    const Item = <MovableItem setIsFirstColumn={setIsFirstColumn}/>;
+
+    return (
+        <div className="container">
+            {/* Wrap components that will be "draggable" and "droppable" */}
+            <DndProvider backend={HTML5Backend}>
+                <Column title='Column 1' className='column first-column'>
+                    {isFirstColumn && Item}
+                </Column>
+                <Column title='Column 2' className='column second-column'>
+                    {!isFirstColumn && Item}
+                </Column>
+            </DndProvider>
+        </div>
+    );
+}
